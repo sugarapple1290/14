ff8597f441866151d381b87dfa1fb53d467b4301
sugarapple1290 <zeeshanhadiya4@gmail.com>
2025-10-05 03:43:02 -0700

    1

---
+++ activity1.py
@@ -0,0 +1,41 @@
+#to ask a user for their score in 5 different subject and calculate the total of it
+'''math=int(input("what is your score in math: "))
+english=int(input("what is your score in english: "))
+arabic=int(input("what is your score in arabic: "))
+python=int(input("what is your score in python: "))
+ss=int(input("what is your score in ss: "))
+print(english+math+arabic+python+ss)'''
+
+#ask user for their score in float
+'''math=float(input("what is your math score: "))
+english=float(input("what is your english score: "))
+arabic=float(input("what is your arabic score: "))
+python=float(input("what is your python score: "))
+ss=float(input("what is your ss score: "))
+print(english+math+arabic+python+ss)'''
+
+#to ask the user for their choice of cake and return the total bill amount
+'''count=int(input("how many slices of chocolate cake do you want: "))
+print(count)
+totalcost=10*count
+print(totalcost)'''
+
+#Convert the following into boolean: "True", "False", "0", "1".
+a="True"
+b=bool(a)
+print(b)
+print(type(b))
+
+
+a="False "
+b=bool(a)
+print(b)
+print(type(b))
+
+
+a="0"
+b=bool(a)
+print(b)
+print(type(b))
+  
+  
